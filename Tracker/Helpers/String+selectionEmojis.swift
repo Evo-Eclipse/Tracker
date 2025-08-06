4e702df695435b4f63dad18b2609cea4ccfb900a
Evo Eclipse <evo.galaio@outlook.com>
2025-08-06 22:46:07 +0300

    Implement emoji & color selection for Tracker creation
    - Added emoji and color collection views to TrackerCreationFormViewController
    - Created reusable EmojiCollectionViewCell and ColorCollectionViewCell components
    - Added String and UIColor extensions for emoji and color selection data
    - Implemented selection/deselection logic with visual feedback
    - Integrated with existing form validation

---
+++ Tracker/Helpers/String+selectionEmojis.swift
@@ -0,0 +1,15 @@
+//
+//  String+selectionEmojis.swift
+//  Tracker
+//
+//  Created by Pavel Komarov on 06.08.2025.
+//
+
+import UIKit
+
+extension String {
+    static let selectionEmojis: [String] = [
+        "🙂", "😻", "🌺", "🐶", "❤️", "😱", "😇", "😡", "🥶",
+        "🤔", "🙌", "🍔", "🥦", "🏓", "🥇", "🎸", "🏝", "😪",
+    ]
+}
---
+++ Tracker/Helpers/UIColor+selectionColors.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+selectionColors.swift
+//  Tracker
+//
+//  Created by Pavel Komarov on 06.08.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static let selectionColors: [UIColor] = [
+        .ypSelection1, .ypSelection2, .ypSelection3, .ypSelection4, .ypSelection5,
+        .ypSelection6, .ypSelection7, .ypSelection8, .ypSelection9, .ypSelection10,
+        .ypSelection11, .ypSelection12, .ypSelection13, .ypSelection14,
+        .ypSelection15, .ypSelection16, .ypSelection17, .ypSelection18,
+    ]
+}
